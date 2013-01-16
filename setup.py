f727d2e16c4f56fc86cc7b57265d7db99c7baefb
Jason Gerard DeRose <jderose@novacut.com>
2013-01-16 02:16:32 -0700

    Initial Python package stub

---
+++ setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+# dbase32: base32-encoding with a sorted-order alphabet (for databases)
+# Copyright (C) 2013 Novacut Inc
+#
+# This file is part of `dbase32`.
+#
+# `dbase32` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# `dbase32` is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with `dbase32`.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+#
+
+"""
+Install `dbase32`.
+"""
+
+import sys
+if sys.version_info < (3, 2):
+    sys.exit('dbase32 requires Python 3.2 or newer')
+
+from distutils.core import setup
+from distutils.cmd import Command
+from unittest import TestLoader, TextTestRunner
+from doctest import DocTestSuite
+import os
+from os import path
+import subprocess
+from urllib.parse import urlparse
+
+import dbase32
+
+
+class Test(Command):
+    description = 'run unit tests and doc tests'
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        pynames = ['dbase32', 'test_dbase32']
+
+        # Add unit-tests:
+        loader = TestLoader()
+        suite = loader.loadTestsFromNames(pynames)
+
+        # Add doc-tests:
+        for name in pynames:
+            suite.addTest(DocTestSuite(name))
+
+        # Run the tests:
+        runner = TextTestRunner(verbosity=2)
+        result = runner.run(suite)
+        if not result.wasSuccessful():
+            raise SystemExit(2)
+
+
+setup(
+    name='dbase32',
+    description='base32-encoding with a sorted-order alphabet',
+    url='https://launchpad.net/dbase32',
+    version=dbase32.__version__,
+    author='Jason Gerard DeRose',
+    author_email='jderose@novacut.com',
+    license='LGPLv3+',
+    py_modules=['dbase32'],
+    cmdclass={'test': Test},
+)
---
+++ test_dbase32.py
@@ -0,0 +1,29 @@
+# dbase32: base32-encoding with a sorted-order alphabet (for databases)
+# Copyright (C) 2013 Novacut Inc
+#
+# This file is part of `dbase32`.
+#
+# `dbase32` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# `dbase32` is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with `dbase32`.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+#
+
+"""
+Unit tests for `dbase32` module.
+"""
+
+from unittest import TestCase
+
+import dbase32
