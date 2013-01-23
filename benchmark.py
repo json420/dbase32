06ead51b532e0c84b9ccddf21f5e94bc0ff57259
Jason Gerard DeRose <jderose@novacut.com>
2013-01-22 22:13:18 -0700

    tweaked benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 # dbase32: base32-encoding with a sorted-order alphabet (for databases)
 # Copyright (C) 2013 Novacut Inc
@@ -22,46 +22,44 @@
 #   Jason Gerard DeRose <jderose@novacut.com>
 #
 
+"""
+Benchmark the db32enc(), db32dec() C implementation.
+"""
+
 import timeit
 
 setup = """
-
 import os
-from base64 import b32encode, b32decode, b64encode, b64decode
+from base64 import b64encode, b64decode
 
-from dbase32 import db32enc_p, db32enc_c, db32dec_p, db32dec_c
+from dbase32 import db32enc, db32dec
+
 
 data = os.urandom(60)
-text_b32 = b32encode(data)
 text_b64 = b64encode(data)
-text_db32 = db32enc_p(data)
+text_db32 = db32enc(data)
 
-assert b32decode(text_b32) == data
 assert b64decode(text_b64) == data
-assert db32dec_p(text_db32) == data
-assert db32dec_c(text_db32) == data
+assert db32dec(text_db32) == data
 """
 
-def run(statement, slow=False):
-    n = 50 * 1000 if slow else 2500 * 1000
+N = 2500 * 1000
+
+def run(statement):
     t = timeit.Timer(statement, setup)
-    elapsed = t.timeit(n)
-    rate = int(n / elapsed)
+    elapsed = t.timeit(N)
+    rate = int(N / elapsed)
     print('{:>14,}: {}'.format(rate, statement))
 
 
 print('')
 
 print('Encodes/second:')
-run('b32encode(data)', slow=True)
-run('db32enc_p(data)', slow=True)
 run('b64encode(data)')
-run('db32enc_c(data)')
+run('db32enc(data)')
 print('')
 
 print('Decodes/second:')
-run('b32decode(text_b32)', slow=True)
-run('db32dec_p(text_db32)', slow=True)
 run('b64decode(text_b64)')
-run('db32dec_c(text_db32)')
+run('db32dec(text_db32)')
 print('')
