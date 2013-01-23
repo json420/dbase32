f317e7de10e93e1640b63a799933ce6180f6e855
Jason Gerard DeRose <jderose@novacut.com>
2013-01-22 19:41:42 -0700

    Reworked gen.py to use functions in misc, generate full reverse-tables

--- gen.py
+++ gen.py
@@ -1,124 +1,69 @@
-from collections import namedtuple
+#!/usr/bin/python3
 
-Encoding = namedtuple('Encoding', 'name removed title desc start end forward reverse')
+# dbase32: base32-encoding with a sorted-order alphabet (for databases)
+# Copyright (C) 2013 Novacut Inc
+#
+# This file is part of `dbase32`.
+#
+# `dbase32` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# `dbase32` is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with `dbase32`.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+#
 
-possible = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-assert ''.join(sorted(set(possible))) == possible
+import optparse
 
-
-def build_forward(remove):
-    remove = set(remove)
-    assert len(remove) == 4
-    assert remove.issubset(possible)
-    forward = set(possible) - remove
-    assert len(forward) == 32
-    return ''.join(sorted(forward))
-
-
-def build_reverse_iter(forward):
-    start = ord(min(forward))
-    stop = ord(max(forward)) + 1
-    for (i, d) in enumerate(range(start, stop)):
-        c = chr(d)
-        r = forward.find(c)
-        assert r < 32
-        if r < 0:
-            r = 255
-        yield (r, d, c, i)
-
-
-def build_reverse(forward):
-    assert len(set(forward)) == len(forward)
-    assert set(forward).issubset(possible)
-    return tuple(build_reverse_iter(forward))
+import dbase32
+from dbase32 import misc
 
 
-def iter_c(t):
-    name = t.name.upper()
-    yield '// {}: {}: {}'.format(name, t.title, t.desc)
-    yield '// [removes {}]'.format(', '.join(l for l in t.removed))
-    yield 'static const uint8_t {}_START = {!r};'.format(t.name.upper(), t.start)
-    yield 'static const uint8_t {}_END = {!r};'.format(t.name.upper(), t.end)
-    yield 'static const uint8_t {}_FORWARD[{}] = "{}";'.format(
-        name, len(t.forward), t.forward
-    )
-    yield 'static const uint8_t {}_REVERSE[{}] = {{'.format(
-        name, len(t.reverse)
-    )
-    for (r, d, c, i) in t.reverse:
-        yield '    {:>3},  // {} {!r} [{:>2}]'.format(r, d, c, i)
-    yield '};'
+REMOVE = '012Z'
+NAME = 'db32'
 
 
-def iter_python(t):
-    name = t.name.upper()
-    yield '# {}: {}: {}'.format(name, t.title, t.desc)
-    yield '# [removes {}]'.format(', '.join(l for l in t.removed))
-    yield '{}_START = {!r}'.format(name, t.start)
-    yield '{}_END = {!r}'.format(name, t.end)
-    yield '{}_FORWARD = {!r}'.format(name, t.forward)
-    yield '{}_REVERSE = ('.format(name)
-    for (r, d, c, i) in t.reverse:
-        yield '    {:>3},  # {} {!r} [{:>2}]'.format(r, d, c, i)
-    yield ')'
-
-
-def build_encoding(name, remove, title, desc, forward=None):
-    if forward is None:
-        forward = build_forward(remove)
-    assert set(possible) - set(remove) == set(forward)
-    assert len(set(remove)) == 4
-    assert set(remove).issubset(possible)
-    start = ord(min(forward))
-    end = ord(max(forward))
-    reverse = build_reverse(forward)
-    return Encoding(name, remove, title, desc, start, end, forward, reverse)
-
-
-def print_python(*encodings):
-    for enc in encodings:
-        for line in iter_python(enc):
-            print(line)
-        print('')
-
- 
-def print_c(*encodings):
-    for enc in encodings:
-        for line in iter_c(enc):
-            print(line)
-        print('')
-
-
-b32 = build_encoding('b32', '0189',
-    'RFC-3548', 'different binary vs encoded sort order (deal breaker!)',
-    forward='ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+parser = optparse.OptionParser(
+    usage='%prog',
+	version=dbase32.__version__,
 )
-
-sb32 = build_encoding('sb32', '0189',
-    'Sorted-Base32', 'standard RFC-3548 letters, but in sorted order'
+parser.add_option('-r',
+    dest='remove',
+    help='symbols to remove; default is {!r}'.format(REMOVE),
+    default=REMOVE,
 )
-
-db32 = build_encoding('db32', '012Z',
-    'Dmedia-Base32', 'non-standard 3-9, A-Y letters'
+parser.add_option('-n',
+    dest='name',
+    help='encoding name; default is {!r}'.format(NAME),
+    default=NAME,
 )
-
-izob32 = build_encoding('izob32', 'EMOC',
-    'Base32-by-IZO', 'The great bearded one speaketh'
+parser.add_option('-p',
+    dest='python',
+    help='generate Python tables (instead of C)',
+    action='store_true',
+    default=False,
 )
-
-hb32 = build_encoding('hb32', 'DMAN',
-    'Hippie-Base32', 'max unique-snowflake units away from RFC-3548'
-)
-
-zb32 = build_encoding('zb32', '0lv2'.upper(),
-    'z-base-32',
-    'http://philzimmermann.com/docs/human-oriented-base-32-encoding.txt'
-)
+(options, args) = parser.parse_args()
 
 
-encodings = (b32, sb32, db32, zb32)
-for enc in encodings:
-    print_c(enc)
-for enc in encodings:
-    print_python(enc)
+forward = misc.gen_forward(options.remove)
+reverse = misc.gen_reverse(forward)
+start = misc.get_start(forward)
+end = misc.get_end(forward)
+name = options.name.upper()
+line_iter = (misc.iter_python if options.python else misc.iter_c)
 
+
+print('')
+for line in line_iter(name, forward, reverse, start, end):
+    print(line)
+print('')
