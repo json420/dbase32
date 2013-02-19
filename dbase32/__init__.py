7cfebd2495a4b9c97be25d6840df541fcbc73f34
Jason Gerard DeRose <jderose@novacut.com>
2013-02-19 14:23:23 -0700

    Bumped version to 0.5.0

--- dbase32/__init__.py
+++ dbase32/__init__.py
@@ -39,7 +39,7 @@ except ImportError:
     from .fallback import db32enc, db32dec, isdb32, check_db32, random_id
 
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __all__ = ('db32enc', 'db32dec', 'isdb32', 'check_db32', 'random_id')
 
 DB32ALPHABET = '3456789ABCDEFGHIJKLMNOPQRSTUVWXY'
