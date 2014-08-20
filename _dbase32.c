3a8cb5bde1d9eb7aade4e8b85fcd8ae56de31c05
Jason Gerard DeRose <jason@system76.com>
2014-08-20 00:29:17 -0600

    Fix comment style in _dbase32.c header

--- _dbase32.c
+++ _dbase32.c
@@ -1,25 +1,24 @@
-/*
-dbase32: base32-encoding with a sorted-order alphabet (for databases)
-Copyright (C) 2013 Novacut Inc
-
-This file is part of `dbase32`.
-
-`dbase32` is free software: you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your option) any
-later version.
-
-`dbase32` is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with `dbase32`.  If not, see <http://www.gnu.org/licenses/>.
-
-Authors:
-    Jason Gerard DeRose <jderose@novacut.com>
-*/
+/* dbase32: base32-encoding with a sorted-order alphabet (for databases)
+ * Copyright (C) 2013 Novacut Inc
+ *
+ * This file is part of `dbase32`.
+ *
+ * `dbase32` is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * `dbase32` is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with `dbase32`.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *    Jason Gerard DeRose <jderose@novacut.com>
+ */
 
 #include <Python.h>
 
