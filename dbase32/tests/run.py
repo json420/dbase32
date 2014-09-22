2bbd013a02cac9b609652f9f0d7f58df74f95601
Jason Gerard DeRose <jason@system76.com>
2014-09-22 15:41:24 -0600

    dbase32.tests.run now dynamically builds pynames, so new or renamed modules aren't missed by mistake

--- dbase32/tests/run.py
+++ dbase32/tests/run.py
@@ -25,25 +25,50 @@ Run `dbase32` unit tests.
 """
 
 import sys
+import os
 from os import path
+import stat
 from unittest import TestLoader, TextTestRunner
 from doctest import DocTestSuite
 
 import dbase32
 
-pynames = (
-    'dbase32',
-    'dbase32._dbase32py',
-    'dbase32.rfc3548',
-    'dbase32.gen',
-    'dbase32.tests',
-    'dbase32.tests.test__dbase32py',
-    'dbase32.tests.test_rfc3548',
-    'dbase32.tests.test_gen',
-)
+
+packagedir = path.dirname(path.abspath(dbase32.__file__))
+
+
+def pynames_iter(pkdir=packagedir, pkname=None):
+    """
+    Recursively yield dotted names for *.py files in directory *pydir*.
+    """
+    if not path.isfile(path.join(pkdir, '__init__.py')):
+        return
+    if pkname is None:
+        pkname = path.basename(pkdir)
+    yield pkname
+    dirs = []
+    for name in sorted(os.listdir(pkdir)):
+        if name in ('__init__.py', '__pycache__'):
+            continue
+        if name.startswith('.') or name.endswith('~'):
+            continue
+        fullname = path.join(pkdir, name)
+        st = os.lstat(fullname)
+        if stat.S_ISREG(st.st_mode) and name.endswith('.py'):
+            parts = name.split('.')
+            if len(parts) == 2:
+                yield '.'.join([pkname, parts[0]])
+        elif stat.S_ISDIR(st.st_mode):
+            dirs.append((fullname, name))
+    for (fullname, name) in dirs:
+        subpkg = '.'.join([pkname, name])
+        for n in pynames_iter(fullname, subpkg):
+            yield n
 
 
 def run_tests():
+    pynames = tuple(pynames_iter())
+
     # Add unit-tests:
     loader = TestLoader()
     suite = loader.loadTestsFromNames(pynames)
